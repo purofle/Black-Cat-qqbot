6594c9490f04073968d5cfa981983a15acb20bbc
purofle <3272912942@qq.com>
2021-02-19 19:25:48 +0800

    让yaml变得更安全

--- functions/keys/key.py
+++ functions/keys/key.py
@@ -3,7 +3,7 @@ import yaml
 def read(path: str):
     with open(path, "r") as f:
         n = f.read()
-    return yaml.load(n)
+    return yaml.safe_load(n)
 
 if __name__ == "__main__":
     save("config.yaml")
--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-from graia.saya import Saya
-from graia.broadcast import Broadcast
-from graia.saya.builtins.broadcast import BroadcastBehaviour
+from apps import app
+import functions
 
-import asyncio
-
-loop = asyncio.get_event_loop()
-bcc = Broadcast(loop=loop)
-saya = Saya(bcc) # 这里可以置空, 但是会丢失 Lifecycle 特性
-
-saya.install_behaviours(BroadcastBehaviour(bcc))
+app.launch_blocking()
