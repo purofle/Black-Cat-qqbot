ab43ccf98a410aad679cac7d32aa9bb20ec2af10
purofle <3272912942@qq.com>
2021-03-20 17:48:35 +0800

    saya

--- functions/coolapk/__init__.py
+++ functions/coolapk/__init__.py
@@ -5,44 +5,44 @@ from graia.application.entry import GroupMessage
 from graia.application.group import Group, Member
 from graia.application.message.chain import MessageChain
 from graia.application.message.elements.internal import At, Image, Plain
+from graia.application.message.parser.kanata import Kanata
+from graia.application.message.parser.signature import FullMatch
 from graia.saya import Channel, Saya
 from graia.saya.builtins.broadcast.schema import ListenerSchema
-from graia.saya.event import SayaModuleInstalled
 
-from functions.coolapk.coolapk_token import request
+from .coolapk_token import request
+
+__name__ = "coolapk"
+__description__ = "获取酷安酷图"
+__author__ = "purofle"
+__usage__ = "在群内发送 酷图 即可"
 
 saya = Saya.current()
 channel = Channel.current()
 
-@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+channel.name(__name__)
+channel.author(__author__)
+channel.description(f"{__description__}\n使用方法：{__usage__}")
+
+@channel.use(ListenerSchema(listening_events=[GroupMessage],inline_dispatchers=[Kanata([FullMatch("酷安酷图")])]))
 async def coolapk(
         app: GraiaMiraiApplication,
-        message: MessageChain,
         group: Group,
         member: Member
         ):
-    if message.asDisplay() == "coolapk":
+    await app.sendGroupMessage(group, MessageChain.create([
+        At(member.id),
+        Plain(" 图片发送较慢，请稍等...")
+        ]))
 
-        await app.sendGroupMessage(group, MessageChain.create([
-            At(member.id),
-            Plain(" 图片发送较慢，请稍等...")
-            ]))
+    n = await request("https://api.coolapk.com/v6/page/dataList?url=%2Ffeed%2FcoolPictureList%3FfragmentTemplate%3Dflex&title=&subTitle=&page=1")
 
-        n = await request("https://api.coolapk.com/v6/page/dataList?url=%2Ffeed%2FcoolPictureList%3FfragmentTemplate%3Dflex&title=&subTitle=&page=1&firstItem=24272618")
+    choice_dict = random.choice(n)
 
-        choice_dict = random.choice(n)
+    url = choice_dict["pic"]
+    username = choice_dict["username"]
+    tag = choice_dict["tags"]
+    device = choice_dict["device_title"]
 
-        url = choice_dict["pic"]
-        username = choice_dict["username"]
-        tag = choice_dict["tags"]
-        device = choice_dict["device_title"]
-
-        msg = [
-                At(member.id),
-                Plain(""" 用户名：{}
-使用设备：{}
-标签：{}""".format(username, device, tag)),
-                Image.fromNetworkAddress(url)
-                ]
-
-        await app.sendGroupMessage(group, MessageChain.create(msg))
+    msg = [At(member.id),Plain("用户名：{}\n使用设备：{}\n标签：{}".format(username, device, tag)),Image.fromNetworkAddress(url)]
+    await app.sendGroupMessage(group, MessageChain.create(msg))
