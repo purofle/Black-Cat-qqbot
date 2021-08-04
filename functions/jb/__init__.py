12ddc15f4286d97e09d049be0733c6dfe00bcb5f
purofle <3272912942@qq.com>
2021-08-04 13:25:48 +0800

    新增jb排行榜

---
+++ functions/jb/__init__.py
@@ -0,0 +1,44 @@
+from graia.application import GraiaMiraiApplication
+from graia.application.entry import GroupMessage
+from graia.application.group import Group, Member
+from graia.application.message.chain import MessageChain
+from graia.application.message.elements.internal import At, Image, Plain
+from graia.saya import Channel, Saya
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+saya = Saya.current()
+channel = Channel.current()
+
+jb = {}
+
+
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def jba(
+    app: GraiaMiraiApplication, group: Group, member: Member, message: MessageChain
+):
+    if message.has(Image):
+        print(message.get(Image)[0].imageId)
+        if (
+            message.get(Image)[0].imageId
+            == "{7F7177D2-D24A-93F5-32BA-C50CCFD02F70}.jpg"
+        ):
+            if not jb.get(group.id):
+                jb[group.id] = {}
+            if not jb[group.id].get(member.id):
+                jb[group.id][member.id] = 0
+            jb[group.id][member.id] += 1
+            await app.sendGroupMessage(
+                group, MessageChain.create([At(member.id), Plain(" 你的jb-1")])
+            )
+
+
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def jbTop(
+    app: GraiaMiraiApplication, group: Group, member: Member, message: MessageChain
+):
+    if jb.get(group.id):
+        if message.asDisplay() == "jb排行榜":
+            await app.sendGroupMessage(
+                group,
+                MessageChain.create([Plain("jb排行榜：\n{}".format(str(jb[group.id])))]),
+            )
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 import glob
 
 
@@ -8,3 +9,18 @@ def get_all_package_name(path: str) -> list[str]:
         return os.path.isdir(d) and glob.glob(os.path.join(d, "__init__.py"))
 
     return list(filter(is_package, os.listdir(path)))
+
+
+def uncaught_error_handler(cls, ex):
+    print("?")
+    root = os.getcwd().rsplit(os.sep)[-1]
+    print(f"{ex['exception'].__class__.__name__}: {ex['exception']}")
+    for i in traceback.extract_tb(ex["exception"].__traceback__):
+        obj_name = (
+            "bot"
+            + i.filename.rstrip(".py").rsplit(root, 1)[1].replace(os.sep, ".")
+            + "."
+            + i.name
+        )
+        filename = i.filename.rsplit(os.sep, 1)[1]
+        print(f"\nat {obj_name}({filename}:{i.lineno})")
