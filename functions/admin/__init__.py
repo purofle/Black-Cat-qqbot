008242df689ac824d6846d9751be17e1e85abbff
purofle <3272912942@qq.com>
2021-08-05 20:11:31 +0800

    新增禁言

--- functions/admin/__init__.py
+++ functions/admin/__init__.py
@@ -13,29 +13,25 @@ import random
 saya = Saya.current()
 channel = Channel.current()
 
-@channel.use(ListenerSchema(listening_events=[GroupMessage]),
-inline_dispatchers=[Kanata([FullMatch("随机禁言")])])
+@channel.use(
+        ListenerSchema(
+            listening_events=[GroupMessage],
+            inline_dispatchers=[Kanata([FullMatch("随机禁言")])]
+            )
+        )
 async def jy(
     app: GraiaMiraiApplication,
     group: Group,
     member: Member):
-    if group.accountPerm != MemberPerm.Administrator and member.permission == MemberPerm.Administrator:
-        await app.sendGroupMessage(
-            group,
-            MessageChain.create([
-                Plain("管理员？拿来把你")
-            ]))
-        return
-    if not group.accountPerm == MemberPerm.Administrator:
-        await app.sendGroupMessage(
-            group,
-            MessageChain.create([
-                Plain("没管理禁nm")
-            ]))
-    else:
-        randint = random.randint(1,60)
+    randint = random.randint(1,60)
+    await app.sendGroupMessage(group, MessageChain.create([
+        At(member.id),
+        Plain(f" 恭喜{member.name}获得{randint}秒的禁言时间！")
+    ]))
+    try:
+        await app.mute(group, member, randint)
+    except PermissionError:
         await app.sendGroupMessage(group, MessageChain.create([
             At(member.id),
-            Plain(f" 恭喜{member.name}获得{randint}秒的禁言时间！")
-        ]))
-        await app.mute(group, member, randint)
+            Plain(" 没管理禁nm")
+            ]))
